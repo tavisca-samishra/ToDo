6e4b162b7f69f969276a56686cdc487e1931ba59
samishra <samishra@tavisca.in>
2019-08-23 14:09:59 +0530

    added search and json

--- behavior.js
+++ behavior.js
@@ -1,14 +1,31 @@
 function check() {
     var username = document.getElementById("user").value;
     var password = document.getElementById("pass").value;
-    if (username == "saurabh") {
-        if (password == "1234")
-            document.getElementById("status").innerHTML = "LOGGED IN";
-        else
-            document.getElementById("status").innerHTML = "Invalid";
-    }
-    else
-        document.getElementById("status").innerHTML = "Invalid";
+    document.getElementById("log").style.display = 'none';
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(ab => {
+            if (password == "") {
+                for (var i = 0; i < ab.length; i++) {
+                    if (ab[i].userId == username) {
+                        var row = document.getElementById("tb").insertRow();
+                        row.insertCell(0).innerHTML = ab[i].title;
+                        row.insertCell(1).innerHTML = "<button class=\"button\" onclick=\"deleteTodo(this)\">X</button>";
+                        row.insertCell(2).innerHTML = "<button class=\"button\" onclick=\"editRow(this)\">Edit</button>";
+                    }
+                }
+            }
+            else {
+                for (var i = 0; i < ab.length; i++) {
+                    if (ab[i].userId == username && ab[i].id == password) {
+                        var row = document.getElementById("tb").insertRow();
+                        row.insertCell(0).innerHTML = ab[i].title;
+                        row.insertCell(1).innerHTML = "<button class=\"button\" onclick=\"deleteTodo(this)\">X</button>";
+                        row.insertCell(2).innerHTML = "<button class=\"button\" onclick=\"editRow(this)\">Edit</button>";
+                    }
+                }
+            }
+        });
 }
 function addRow() {
     var input = document.getElementById("input").value;
@@ -43,4 +60,18 @@ function append(r) {
     row.insertCell(1).innerHTML = "<button class=\"button\" onclick=\"deleteTodo(this)\">X</button>";
     row.insertCell(2).innerHTML = "<button class=\"button\" onclick=\"editRow(this)\">Edit</button>";
     document.getElementById("tb").deleteRow(i + 1);
+}
+function search() {
+    var input = document.getElementById("input").value;
+    var x = document.getElementById("tb").rows.length;
+    var data;
+    for (let i = 0; i < x; i++) {
+        data = document.getElementById("tb").rows[i].cells[0].innerHTML;
+        if (data.indexOf(input) > -1) {
+            document.getElementById("tb").rows[i].style.display = "";
+        }
+        else {
+            document.getElementById("tb").rows[i].style.display = "none";
+        }
+    }
 }
